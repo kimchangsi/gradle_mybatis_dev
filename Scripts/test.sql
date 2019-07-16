aa71d508f47e626afb1eb8d7812571b34746b8f6
김창민 <kkccmm456@naver.com>
2019-07-16 18:04:30 +0900

    TypeHandler test complete

--- Scripts/test.sql
+++ Scripts/test.sql
@@ -5,3 +5,11 @@ SELECT *FROM course_enrollment;
 SELECT *FROM courses;
 SELECT *FROM students;
 SELECT *FROM tutors;
+
+
+select stud_id ,name,email,dob,phone
+from students where stud_id=1;
+
+select stud_id ,name,email,dob,phone,
+substring(phone,1,3) as f,substring(phone,5,3) as m,substring(phone,9,4) as l
+from students where stud_id=1;
---
+++ src/main/java/kr/or/yi/gradle_mybatis_dev/dao/StudentMapperImpl.java
@@ -0,0 +1,23 @@
+package kr.or.yi.gradle_mybatis_dev.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.yi.gradle_mybatis_dev.dto.Student;
+import kr.or.yi.gradle_mybatis_dev.jdbc.MybatisSqlSessionFactory;
+
+public class StudentMapperImpl implements StudentMapper {
+	private String namespace = "kr.or.yi.gradle_mybatis_dev.dao.StudentMapper";
+	@Override
+	public Student selectStudentByNo(Student student) {
+		try(SqlSession sqlSession = MybatisSqlSessionFactory.openSession()){
+			return sqlSession.selectOne(namespace + ".selectStudentByNo", student);
+		}
+	}
+	@Override
+	public Student selectStudentByNoWithResultMap(Student student) {
+		try(SqlSession sqlSession = MybatisSqlSessionFactory.openSession()){
+			return sqlSession.selectOne(namespace + ".selectStudentByNoWithResultMap", student);
+		}
+	}
+
+}
